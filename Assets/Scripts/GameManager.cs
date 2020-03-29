d842ddc1adafe83d2251638441198cd48522cc0d
Brian Leip <brianleip@gmail.com>
2020-03-28 18:52:04 -0700

    created GameManager singleton

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region PUBLIC VARIABLES
+    public static GameManager instance {get; private set;}  // singleton for GameManager
+    #endregion
+
+    #region PRIVATE VARIABLES
+
+    #endregion
+
+    public void Awake() {
+        if (instance==null) {
+            instance = this;
+        }
+        else {
+            Debug.Log("Warning, duplicate game manager.  Destroying one");
+            Destroy(instance);
+            instance = this;
+        }
+        DontDestroyOnLoad(instance);
+    }
+}
---
+++ Assets/Scripts/Person.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person : MonoBehaviour
+{
+    #region PUBLIC VARIABLES
+    public float speed;
+    #endregion
+
+    #region PRIVATE VARIABLES
+
+    #endregion
+
+
+}
