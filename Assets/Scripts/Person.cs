445500d8c61cb39dce770db34c7aaa940dada773
Brian Leip <brianleip@gmail.com>
2020-03-28 21:29:43 -0700

    Person turning left, right, 180

--- Assets/Scripts/Person.cs
+++ Assets/Scripts/Person.cs
@@ -5,30 +5,62 @@ using UnityEngine;
 public class Person : MonoBehaviour
 {
     #region PUBLIC VARIABLES
+    public float moveSpeed {get; private set;}
+    public enum PersonState {idle, walking, running, sitting, sheltered}
     #endregion
-        public float moveSpeed {get; private set;}
+
     #region PRIVATE VARIABLES
-        private Animator animator;
-        private Rigidbody rb; 
-        private float speedAnimAdj;
-
+    private Animator animator;
+    private Rigidbody rb; 
+    private PersonState state = PersonState.idle;
+    private float speedAnimAdj = 5f;    // fixed number to sync animation with movement speed
+    private float targetTime = 0f;
     #endregion
 
+    #region FUNCTIONS
     private void Awake() {
         animator = GetComponentInChildren<Animator>();
         rb = GetComponent<Rigidbody>();
-        speedAnimAdj = 3f;
+    }
+
+    private void Update() {
+        switch(state) {
+            case PersonState.idle:
+                StartWalking();    // TODO: REPLACE THIS
+                break;
+            case PersonState.walking:
+                if(Mathf.Round(Time.timeSinceLevelLoad % 2) == 0) {
+                    Turn(90f);
+                }
+                break;
+            case PersonState.running:
+                break;
+            default:
+                StartWalking();    // TODO: REPLACE THIS
+                break;
+        }
     }
 
     private void FixedUpdate() {
-        if (moveSpeed < 1) {
-            moveSpeed += 0.002f;
-            animator.SetFloat("Speed_f", moveSpeed);
-        }
-        else {
-            moveSpeed = 0;
-            animator.SetFloat("Speed_f", moveSpeed);
-        }
+        // Move Person
         rb.MovePosition(transform.position + transform.forward * moveSpeed * speedAnimAdj * Time.fixedDeltaTime);
     }
+
+    private void StartWalking() {
+        state = PersonState.walking;
+        moveSpeed = .75f;
+        animator.SetFloat("Speed_f", moveSpeed);
+    }
+
+    private void StartRunning() {
+        state = PersonState.running;
+        moveSpeed = 1.5f;
+        animator.SetFloat("Speed_f", moveSpeed);
+    }
+
+    private void Turn(float turnDegrees) {
+        transform.Rotate(0,turnDegrees,0);
+    }
+    #endregion
+
 }
