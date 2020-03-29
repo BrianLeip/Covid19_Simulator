08d1efe25faca19d2b72111bea63e453206e5308
Brian Leip <brianleip@gmail.com>
2020-03-28 19:39:47 -0700

    Progres on Person movement

--- Assets/Scripts/Person.cs
+++ Assets/Scripts/Person.cs
@@ -5,12 +5,30 @@ using UnityEngine;
 public class Person : MonoBehaviour
 {
     #region PUBLIC VARIABLES
-    public float speed;
     #endregion
-
+        public float moveSpeed {get; private set;}
     #region PRIVATE VARIABLES
+        private Animator animator;
+        private Rigidbody rb; 
+        private float speedAnimAdj;
 
     #endregion
 
+    private void Awake() {
+        animator = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody>();
+        speedAnimAdj = 3f;
+    }
 
+    private void FixedUpdate() {
+        if (moveSpeed < 1) {
+            moveSpeed += 0.002f;
+            animator.SetFloat("Speed_f", moveSpeed);
+        }
+        else {
+            moveSpeed = 0;
+            animator.SetFloat("Speed_f", moveSpeed);
+        }
+        rb.MovePosition(transform.position + transform.forward * moveSpeed * speedAnimAdj * Time.fixedDeltaTime);
+    }
 }
